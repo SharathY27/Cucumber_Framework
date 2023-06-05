0a2b58bca6986b7004f41c86e138afa179912553
Sharath <Sharath@DESKTOP-I3V29VC>
2023-06-05 11:06:20 +0530

    Failing and Rerunning scenarios

--- Cucumber_Framework/src/test/java/runner/TestRunner.java
+++ Cucumber_Framework/src/test/java/runner/TestRunner.java
@@ -8,7 +8,15 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 	features = "src/test/resources/features/Verify_CEO_Name.feature",
-	glue = "steps"
+	glue = "steps",
+	monochrome=true,
+	plugin= {"rerun:target/failed_scenarios.txt"} // this plugin is used to rerun the failed scenarios
+	
+	
+	//Note : usually target folder is majorly used for storing failed scenarios, if you want to store in customized folder
+//	Example: you are using plugin= {"rerun:failed_scenarios/failed_scenarios.txt"}
+//	this will create the folder under project level named failed_scenarios inside failed_scenarios.txt will be created 
+//	In Rerunner class in features we need to use @failed_scenarios instead of target if using customized folder
 )
 public class TestRunner {
 
--- Cucumber_Framework/src/test/resources/features/Verify_CEO_Name.feature
+++ Cucumber_Framework/src/test/resources/features/Verify_CEO_Name.feature
@@ -1,11 +1,9 @@
 @GetCEOName
 Feature: This feature is to get the CEO name from OrangeHRM app
-Scenario Outline: This test is to verify CEO Name
-	Given the user is logged in successfully and is on HomePage
-	When the user clicks on the directory option from the Menu bar
-	And the user selects the job title as "CEO" from the dropdown
-	And clicks the search button
-	Then the user should see the CEO name as "<CEO_Name>"
-Examples:
-|CEO_Name|
-|John Smith|
+
+  Scenario Outline: This test is to verify CEO Name
+    Given the user is logged in successfully and is on HomePage
+    When the user clicks on the directory option from the Menu bar
+    And the user selects the job title as "CEO" from the dropdown
+    And clicks the search button
+    Then the user should see the CEO name
